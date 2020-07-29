f37337e4a8021115ec9d373599d6061285f45419
juanpablodejesus <juanpablodejesus_figueroa@ucol.mx>
2020-07-29 03:40:49 -0500

    La bendicion

--- ingles/carga.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load',()=>{
-    setTimeout(cargar,4000);
-    function cargar(){
-    document.getElementById('contenido').style.display = "inherit";
-    document.getElementById('carga').style.display = "none"; 
-    }
-    
-})
